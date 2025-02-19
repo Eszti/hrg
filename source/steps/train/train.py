cab620b34b2dbfb2d3dff8e299038a9f255d350b
Eszti <eszter.iklodi@gmail.com>
2025-02-19 10:14:47 +0100

    Add validate option for train

--- source/steps/train/train.py
+++ source/steps/train/train.py
@@ -24,6 +24,7 @@ class Train(LoopOnTriplets):
             config=config,
         )
         self.method = self.config["method"]
+        self.validate = self.config.get("validate", False)
         self.out_dir += f"_{self.method}"
         self.no_rule = []
         self.not_validated = []
@@ -50,49 +51,54 @@ class Train(LoopOnTriplets):
             for rule in sorted(rules):
                 grammar_lines.append(f"{rule}")
             triplet_logger.log(f"Grammar length: {len(grammar_lines)}")
-            grammar = Grammar.load_from_file(
-                grammar_lines, VoRule, nodelabels=True, logprob=True
-            )
-            parser = Parser(grammar, stop_at_first=True, permutations=False)
 
-            try:
-                derivation = parser.check_membership(triplet_graph_str, triplet_logger)
-                if derivation is None:
-                    self.not_validated.append(triplet_idx)
-                number_of_used_rule = len(derivation.rules_counter.keys())
-                if number_of_used_rule != len(grammar):
-                    triplet_logger.log(
-                        f"\nNot all rules are used: {number_of_used_rule} of {len(grammar)}\n"
+            if self.validate:
+                grammar = Grammar.load_from_file(
+                    grammar_lines, VoRule, nodelabels=True, logprob=True
+                )
+                parser = Parser(grammar, stop_at_first=True, permutations=False)
+
+                try:
+                    derivation = parser.check_membership(
+                        triplet_graph_str, triplet_logger
                     )
-                    self.not_all_rules_used.append(triplet_idx)
-                with open(f"{hrg_dir}/sen{triplet_idx}.hrg", "w") as f:
-                    f.writelines(grammar_lines)
-            except ParseTooLongException as e:
-                self.parse_did_not_finish.append(triplet_idx)
-                triplet_logger.log(e.print_message())
-            except CkyTooLongException as e:
-                self.cky_did_not_finish.append(triplet_idx)
-                triplet_logger.log(e.print_message())
-            except NotAllNodesCoveredException as e:
-                self.not_all_nodes_covered.append(triplet_idx)
-                triplet_logger.log(e.print_message())
+                    if derivation is None:
+                        self.not_validated.append(triplet_idx)
+                    number_of_used_rule = len(derivation.rules_counter.keys())
+                    if number_of_used_rule != len(grammar):
+                        triplet_logger.log(
+                            f"\nNot all rules are used: {number_of_used_rule} of {len(grammar)}\n"
+                        )
+                        self.not_all_rules_used.append(triplet_idx)
+                    with open(f"{hrg_dir}/sen{triplet_idx}.hrg", "w") as f:
+                        f.writelines(grammar_lines)
+                except ParseTooLongException as e:
+                    self.parse_did_not_finish.append(triplet_idx)
+                    triplet_logger.log(e.print_message())
+                except CkyTooLongException as e:
+                    self.cky_did_not_finish.append(triplet_idx)
+                    triplet_logger.log(e.print_message())
+                except NotAllNodesCoveredException as e:
+                    self.not_all_nodes_covered.append(triplet_idx)
+                    triplet_logger.log(e.print_message())
 
     def _after_loop(self):
-        self.logger.log(
-            f"\nNumber of no rules: {len(self.no_rule)}\n"
-            f"{json.dumps(self.no_rule)}"
-            f"\nNumber of not validated: {len(self.not_validated)}\n"
-            f"{json.dumps(self.not_validated)}"
-            f"\nNumber of not all rules used: {len(self.not_all_rules_used)}\n"
-            f"{json.dumps(self.not_all_rules_used)}"
-            f"\nNumber of not all nodes covered: {len(self.not_all_nodes_covered)}\n"
-            f"{json.dumps(self.not_all_nodes_covered)}"
-            f"\nNumber of parse did not finish: {len(self.parse_did_not_finish)}\n"
-            f"{json.dumps(self.parse_did_not_finish)}"
-            f"\nNumber of cky conversion did not finish: {len(self.cky_did_not_finish)}\n"
-            f"{json.dumps(self.cky_did_not_finish)}",
-            to_stdout=True,
-        )
+        if self.validate:
+            self.logger.log(
+                f"\nNumber of no rules: {len(self.no_rule)}\n"
+                f"{json.dumps(self.no_rule)}"
+                f"\nNumber of not validated: {len(self.not_validated)}\n"
+                f"{json.dumps(self.not_validated)}"
+                f"\nNumber of not all rules used: {len(self.not_all_rules_used)}\n"
+                f"{json.dumps(self.not_all_rules_used)}"
+                f"\nNumber of not all nodes covered: {len(self.not_all_nodes_covered)}\n"
+                f"{json.dumps(self.not_all_nodes_covered)}"
+                f"\nNumber of parse did not finish: {len(self.parse_did_not_finish)}\n"
+                f"{json.dumps(self.parse_did_not_finish)}"
+                f"\nNumber of cky conversion did not finish: {len(self.cky_did_not_finish)}\n"
+                f"{json.dumps(self.cky_did_not_finish)}",
+                to_stdout=True,
+            )
         super()._after_loop()
 
 
