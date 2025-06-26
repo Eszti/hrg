ebc4b7f9d353e364d7b4712fb17a827d9bd8c33d
Eszti <eszter.iklodi@gmail.com>
2025-06-26 10:31:08 +0200

    Prove thesis OpenIE6 - CaRB dataset

--- source/iwcs/carb/oie_readers/openieFiveReader.py
+++ source/iwcs/carb/oie_readers/openieFiveReader.py
@@ -1,5 +1,5 @@
-from oie_readers.oieReader import OieReader
-from oie_readers.extraction import Extraction
+from .oieReader import OieReader
+from .extraction import Extraction
 
 
 class OpenieFiveReader(OieReader):
